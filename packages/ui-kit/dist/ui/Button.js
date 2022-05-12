272fb5edd8ca1dcb0a68e9e7d459b78b676c7600
Francisco Mendoza <frgmendoza@gmail.com>
2022-05-12 00:35:14 -0600

    docker-reactjs

---
+++ packages/ui-kit/dist/ui/Button.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Button = _styledComponents["default"].button.withConfig({
+  displayName: "Button",
+  componentId: "sc-1vpuq1-0"
+})(["background-color:blue;"]);
+
+var _default = Button;
+exports["default"] = _default;
